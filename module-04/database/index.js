a97b95e7b28531274c3b93c6f348bbc676f1e8ec
lucasfrancaid <lucasfrancaid@gmail.com>
2020-06-30 02:43:15 -0300

    Handling files CREATE method with test

--- module-04/database/index.js
+++ module-04/database/index.js
@@ -1,23 +1,42 @@
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
 const { promisify } = require('util');
 
 // Simulating to learn handle other files
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 // Other way for get data of the json file
 // const jsonData = require('./heroes.json'); 
 
 class Database {
     constructor() {
-        this.FILE_NAME = './database/heroes.json'
+        this.FILE_NAME = './database/heroes.json';
     };
+
     async getFileData() {
         const file = await readFileAsync(this.FILE_NAME, 'utf8')
         return JSON.parse(file.toString());
     };
-    writeFile() {
 
-    }
+    async writeFile(data) {
+        await writeFileAsync(this.FILE_NAME, JSON.stringify(data))
+        return true;
+    };
+
+    async create(hero) {
+        const data = await this.getFileData()
+        const id = hero.id <= 2 ? hero.id : Date.now()
+        const heroWithId = { id, ...hero }
+        
+        const updatedData = [
+            ...data,
+            heroWithId
+        ]
+
+        const result = await this.writeFile(updatedData)
+        return result;
+    };
+
     async list(id) {
         const data = await this.getFileData()
         const filteredData = data.filter(item => id ? item.id === id : true)
--- module-04/test.js
+++ module-04/test.js
@@ -2,16 +2,27 @@ const { deepEqual, ok } = require('assert');
 
 const database = require('./database');
 
-const DEFAULT_ITEM_REGISTER = {
+const DEFAULT_ITEM_CREATE = {
     name: 'Flash',
     skill: 'Speed',
     id: 1
 };
 
 describe('Suite of Heroes manipulation', () => {
+    before(async () => {
+        await database.create(DEFAULT_ITEM_CREATE)
+    });
+
     it('Should read a Hero, using files', async () => {
-        const expected = DEFAULT_ITEM_REGISTER
+        const expected = DEFAULT_ITEM_CREATE
         const [result] = await database.list(expected.id)
         deepEqual(result, expected)
     });
+
+    it('Should create a Hero, using files', async () => {
+        const expected = DEFAULT_ITEM_CREATE
+        const result = await database.create(DEFAULT_ITEM_CREATE)
+        const [actual] = await database.list(DEFAULT_ITEM_CREATE.id)
+        deepEqual(actual, expected)
+    })
 });
