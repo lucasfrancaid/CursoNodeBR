a260e8450a80038bba2a186d9e1dbef2c53b7ab6
lucasfrancaid <lucasfrancaid@gmail.com>
2020-07-02 16:22:02 -0300

    Created method Read and added test to read

--- module-07/src/tests/mongodbStrategy.test.js
+++ module-07/src/tests/mongodbStrategy.test.js
@@ -11,18 +11,23 @@ const MOCK_HERO_CREATE = {
 };
 
 describe('MongoDB strategy', function () {
-    this.beforeAll(async function () {
+    this.beforeAll(async () => {
         await context.connect()
     });
 
-    it('MongoDB Connection', async function () {
+    it('MongoDB Connection', async () => {
         const result = await context.isConnected()
         const expected = 'Connected'
         assert.equal(result, expected)
     });
 
-    it('Should create a Hero', async function () {
+    it('Should create a Hero', async () => {
         const { name, skill } = await context.create(MOCK_HERO_CREATE)
+        assert.deepEqual({ name, skill }, MOCK_HERO_CREATE)
+    });
+
+    it('Should read a Hero by name', async () => {
+        const [{ name, skill }] = await context.read({ name: MOCK_HERO_CREATE.name, skill: MOCK_HERO_CREATE.skill })
         assert.deepEqual({ name, skill }, MOCK_HERO_CREATE)
     });
 });
